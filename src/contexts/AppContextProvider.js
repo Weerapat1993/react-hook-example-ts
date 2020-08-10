8776c5729d63c4de8a671dec202f8da82ef19e25
DragonWingZ <miles.gaming.th@gmail.com>
2020-08-10 15:24:00 +0700

    Add Config Logger

--- src/contexts/AppContextProvider.js
+++ src/contexts/AppContextProvider.js
@@ -1,11 +1,12 @@
 import React, { useReducer } from 'react';
 import { initialStore, mainReducer } from '../config/store';
+import { configLogger } from '../config/logger';
 
 // Context API
 export const AppContext = React.createContext(initialStore)
 
 export const AppContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(mainReducer, initialStore)
+  const [state, dispatch] = useReducer(configLogger(mainReducer), initialStore)
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       {children}
